07d00ecbb5628bfe9833692199332504256a0535
SoftpaseFar <kang_hui1314@126.com>
2024-04-09 15:53:28 +0800

    test

--- model.py
+++ model.py
@@ -6,16 +6,6 @@ import torch.nn.functional as F
 class SimpleCNN(nn.Module):
     def __init__(self):
         super(SimpleCNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=5)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=5)
-        self.pool = nn.MaxPool2d(2)
-        self.fc1 = nn.Linear(1024, 128)  # 这里的 1024 取决于前面层的输出
-        self.fc2 = nn.Linear(128, 10)  # 假设有10个输出类别
 
     def forward(self, x):
-        x = F.relu(self.pool(self.conv1(x)))
-        x = F.relu(self.pool(self.conv2(x)))
-        x = torch.flatten(x, 1)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
         return x
