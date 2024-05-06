8c5ee9744de388643be7f77b215761cc940e6ccf
SoftpaseFar <kang_hui1314@126.com>
2024-05-06 16:27:27 +0800

    epochs = 1

--- train_slt.py
+++ train_slt.py
@@ -23,7 +23,7 @@ import multiprocessing
 def get_args_parser():
     a_parser = argparse.ArgumentParser('VLP scripts', add_help=False)
     a_parser.add_argument('--batch_size', default=2, type=int)
-    a_parser.add_argument('--epochs', default=2, type=int)
+    a_parser.add_argument('--epochs', default=1, type=int)
 
     a_parser.add_argument('--config', type=str, default='./config.yaml')
     a_parser.add_argument('--device', default='cuda')
